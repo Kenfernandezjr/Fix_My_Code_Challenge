0613ef8bb91cfd8134ca534d2698f440f7e3dc70
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-04-06 05:22:07 +0000

    Upload test #1, checking if all the passwords error message are correct

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password == hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         print("User.id should be unique")
 
     u_pwd = "myPassword"
-    user_1.__password = u_pwd
+    user_1.password = u_pwd
     if user_1.password == u_pwd:
         print("User.password should be hashed")
 
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if user_1.is_valid_password(u_pwd):
+    if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
